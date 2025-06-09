0267a180bb2548958a432decfa30f7294e63c48b
subin <eltm521@naver.com>
2025-06-09 10:41:39 +0900

    Initial commit

---
+++ src/test/java/com/example/codesquad/CodesquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codesquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
